01a1184cd5b860eaad6295f4c16950773369f209
Alexandra Chicinas <alexandraschicinas@yahoo.com>
2021-02-15 09:46:06 +0200

    improved fn/css and new fn hideHomeP&hideGoogleM

--- functionality.js
+++ functionality.js
@@ -4,12 +4,28 @@ function hide(id) {
     var el = document.getElementById(id);
     el.style.display = "none";
 }
+function hideHomePage() {
+    var header = Array.from(document.getElementsByTagName("header"));
+    var myEl = header.map((el) => {
+        return el.children;
+    })
+    var elArray = Array.from(myEl[0]);
+    let array = elArray.map(el => { 
+        return el.id;
+        });
+    array.forEach(el => hide(el));
+}
+
+function hideGoogleMap() {
+    var map = document.getElementById("map");
+    hide(map.id);
+}
 
 function hideAll() {
     var pages = Array.from(document.querySelectorAll(".page"));
-    pages.forEach(function(page){
+    pages.forEach(function(page) {
         hide(page.id);
-    })
+    });
 }
 function show(id) {
     var pageToshow = document.getElementById(id);
@@ -18,18 +34,23 @@ function show(id) {
 
  function showPage(id) {
     hideAll();
+    hideHomePage();
+    hideGoogleMap()
     show(id);
+
  }
 
-//  function listenMenuClicks(){
-//      document.addEventListener("click", function(e){
-//          var link = e.target;
-//          if(link.matches("#menu a")) {
-//             console.log("click", link.innerHTML.toLowerCase());
-//             var id = link.innerHTML.toLowerCase();
-//             console.log("id" , id)
-//             showPage(id)
-//          }
-//      });
-//  }
-//  listenMenuClicks()
+ function listenMenuClicks(){
+     document.addEventListener("click", function(e){
+         var link = e.target;
+         if(link.matches("#menu a")) {
+            var id = link.getAttribute("data-id");
+            showPage(id)
+         }
+     });
+ }
+ listenMenuClicks()
+
+ function goBack() {
+     location.reload();
+  }
